71c4f6511832ab5ca523a67c17ee9879cb7346d0
Noman Shahzad Memon <112593182+noman693@users.noreply.github.com>
2024-06-05 12:20:15 +0000

    dataTypes done

---
+++ 01_basic/dataType.js
@@ -0,0 +1,33 @@
+"use strict" // use for newly version of javascript syntax
+// alert(3+3) alert now be use in node js enviroment
+// prettier extension use to format your document
+
+console.log( 3 +
+    3
+)   // code readability should me higher
+console.log(3+3);
+
+// for documentation use Mdn and tc39 and tc39 is orignal documentation provide by ECMA
+
+
+let name = "noman shahzad memon";
+let age = 30;
+let Islogin = false;
+let state = null;
+
+
+// number = 2 to power 54
+// string = "0"
+// bigInt = " for long intger values"
+// Boolean = " for true and false"
+// null = standalone value  null dosnt means that it is not define
+// undefined = "not define"
+// Symbol = unique 
+
+
+console.log(typeof "noman");
+console.log(typeof state);
+
+// type of null is object 
+
+// type of undefined variable is undefine 
