54708c564c8a5932495f9569763e37604a59a4df
Noman Shahzad Memon <112593182+noman693@users.noreply.github.com>
2024-06-05 11:37:15 +0000

    variable part done

---
+++ 01_basic/variable.js
@@ -0,0 +1,9 @@
+const acountId = 12345;
+let acountEmail= "nomanmemon@.com";
+var acountPassword = "12345681";
+console.log(acountId);
+console.table([acountEmail,acountId,acountPassword]);
+
+
+// perfer not to use var
+// because of issue in block scope and function scope
